d850e42c7e5126c8c0f04eb2f0c2eab986c739e1
Rostyslav <nucengithub@gmail.com>
2024-11-14 19:43:01 +0200

    Add base css styles and the function changeFilter

--- task-book/src/App.tsx
+++ task-book/src/App.tsx
@@ -1,43 +1,40 @@
 import { useState } from "react";
 import "./index.css"
-import { Todolist } from "./Pages/Todolist";
-import { Task } from "./Pages/Todolist";
+import { Task, Todolist } from "./Pages/Todolist";
 
-
+export type FilterValuesTypes = "all" | "completed" | "active"
 
 function App() {
 
   let WhatToLehrntTasks: Task[] = [
     { id: 1, title: "HTML&CSS", isDone: true },
     { id: 2, title: "JS", isDone: true },
-    { id: 3, title: "React", isDone: true }
-  ]
-
-  let MoviesTasks: Task[] = [
-    { id: 1, title: "Terminator", isDone: true },
-    { id: 2, title: "Joker", isDone: true },
-    { id: 3, title: "Game of Thrones", isDone: true },
-    { id: 4, title: "Simpson's", isDone: true }
-  ]
-
-  let SongsTasks: Task[] = [
-    { id: 1, title: "Lil peep", isDone: true },
-    { id: 2, title: "Imagene dragons", isDone: true },
-    { id: 3, title: "TwentyOnePilot", isDone: true }
+    { id: 3, title: "TS", isDone: false },
+    { id: 4, title: "React", isDone: false }
   ]
 
   let [task, setTask] = useState(WhatToLehrntTasks)
+  let [filter , setFilter] = useState<FilterValuesTypes>("completed")
 
   const removeTask = (id: number) => {
     let FilteredTask = task.filter(t => t.id !== id)
     setTask(FilteredTask)
   }
 
+  const changeFilter = (value: FilterValuesTypes) => {
+    setFilter(value)
+  }
+  
+  let tasksForToDoList = task
+  if (filter === "completed") {
+    tasksForToDoList = task.filter(t => t.isDone === true)
+  } else if (filter === "active") {
+    tasksForToDoList = task.filter(t => t.isDone === false)
+  }
+
   return (
-    <div className="flex">
-      <Todolist title="What to lehrn" tasks={ WhatToLehrntTasks } removeTask={removeTask} />
-      <Todolist title="Movies" tasks={ MoviesTasks } removeTask={removeTask} />
-      <Todolist title="Songs" tasks={ SongsTasks } removeTask={removeTask} />
+    <div className="w-full h-screen content-center">
+      <Todolist title="What to learn?" tasks={ tasksForToDoList } removeTask={removeTask} changeFilter={changeFilter} />
     </div>
   );
 }
--- task-book/src/Pages/Todolist.tsx
+++ task-book/src/Pages/Todolist.tsx
@@ -1,3 +1,5 @@
+import { ALL } from "dns";
+
 export type Task = {
     id: number,
     title: string,
@@ -8,30 +10,37 @@ type PropseType = {
     title: string,
     tasks: Task[], // or Array<Task>
     removeTask: Function,
+    changeFilter: Function,
 }
 
 export const Todolist = (props: PropseType) => {
     return (
-        <div className="">
-            <h3>{ props.title }</h3>
+        <div className="flex flex-col items-center bg-slate-500">
+            <h3 className="text-2xl">{ props.title }</h3>
             <div>
                 <input />
                 <button>+</button>
                 <ul>
                     {
                         props.tasks.map((t) => {
-                            return <li>
+                            return <li className="flex flex-row gap-3">
                                 <input type="checkbox" checked={t.isDone} />
-                                <p>
+                                <p className="w-28 text-xl">
                                     {t.title}
                                 </p>
-                                <button onClick={() => props.removeTask(t.id)}>
+                                <button onClick={() => { props.removeTask(t.id) }}>
                                     x
                                 </button>
                             </li>
                         })
                     }
                 </ul>
+                <div className="flex flex-row gap-3">
+                    <p className="text-lg">Show:</p>
+                    <button className="bg-cyan-600 border-cyan-800 border-2 px-2 text-lg" onClick={ () => { props.changeFilter("all") }}>all</button>
+                    <button className="bg-cyan-600 border-cyan-800 border-2 px-2 text-lg" onClick={ () => {props.changeFilter("active")}}>active</button>
+                    <button className="bg-cyan-600 border-cyan-800 border-2 px-2 text-lg" onClick={ () => {props.changeFilter("completed")}}>completed</button>
+                </div>
             </div>
         </div>
     );
